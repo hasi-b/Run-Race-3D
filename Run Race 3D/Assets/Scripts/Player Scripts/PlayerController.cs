57c61f8ad5347c5b1c30b591d3fd9fbac5c039a5
Mahir-Ashef <hasib.hasan9@gmail.com>
2021-03-09 23:44:29 +0600

    Jump added
    Character controller and jump added

---
+++ Run Race 3D/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 move;
+
+    public float speed,jumpforce,gravity,verticalVelocity;
+
+    private CharacterController charController; 
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        move = Vector3.zero;
+        move = transform.forward;
+
+        if(charController.isGrounded){
+            verticalVelocity = 0;
+            if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+                verticalVelocity = jumpforce;
+
+            }
+        }
+        else
+        {
+            gravity=30;
+            verticalVelocity-= gravity*Time.deltaTime;
+        }
+
+        move.Normalize();
+        move *= speed;
+        move.y = verticalVelocity;
+        charController.Move(move*  Time.deltaTime);
+    }
+}
